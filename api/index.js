d03d182cd5784ac3ae89a2cb3bfd89e5eb7f0ae5
Niraj Chaudhari <nirajchaudhariparshuram@gmail.com>
2024-01-19 18:58:05 +0530

    db connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+try {
+  mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+    console.log("Successfully connected to DB");
+  });
+} catch (error) {
+  console.log(error);
+}
 
 const app = express();
 
