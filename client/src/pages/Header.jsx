017b63208d47fe1991a1ad32936c3d6a45fe2812
Niraj Chaudhari <nirajchaudhariparshuram@gmail.com>
2024-01-21 15:42:20 +0530

    added functionality to sign up page

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -33,7 +33,7 @@ function Header() {
         </Button>
         <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue" outline>
-            SignIn
+            Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
