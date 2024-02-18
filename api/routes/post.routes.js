0375051e63c008b984ef36d7d0840e12d1cbb813
Niraj Chaudhari <nirajchaudhariparshuram@gmail.com>
2024-02-18 23:03:15 +0530

    complete update post functionality with fixed major error in post controller

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { create, deletePost, getpost } from "../controllers/post.controller.js";
+import {
+  create,
+  deletePost,
+  getpost,
+  updatePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, create);
 router.get("/getposts", getpost);
 router.delete("/deletepost/:postId/:userId", verifyUser, deletePost);
+router.put("/updatepost/:postId/:userId", verifyUser, updatePost);
 
 export default router;
